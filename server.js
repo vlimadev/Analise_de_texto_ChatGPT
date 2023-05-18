eb82a022ed49d805588e3d60d1382bbf75d66fb5
vlimadev <vinilimadev@gmail.com>
2023-05-18 18:05:17 -0300

    prompt alteration

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const config = new Configuration({
 const openai = new OpenAIApi(config);
 
 const colmeiIa = async (response) => {
-  const prompt = `Analisar se o texto a seguir tende mais para SIM ou NÃO, se SIM retornar somente CONFIRMAR, se NÃO retornar somente REVER AGENDAMENTO: ${response}`;
+  const prompt = `Analisar se o texto a seguir tende mais para SIM ou NÃO, se SIM retornar somente CONFIRMAR, se NÃO retornar somente REVER AGENDAMENTO caso o sentimento seja neutro retorna NÃO ENTENDI O QUE VOCÊ FALOU: ${response}`;
 
   try {
     const completions = await openai.createCompletion({
@@ -26,7 +26,7 @@ const colmeiIa = async (response) => {
 };
 
 (async () => {
-  const response = "Não estarei em casa";
+  const response = "Pedrão da pedreira pronto para atendelo...";
   const responseBot = await colmeiIa(response);
-  console.log("Resposta:", responseBot.trim());
+  console.log("Resposta:", responseBot);
 })();
